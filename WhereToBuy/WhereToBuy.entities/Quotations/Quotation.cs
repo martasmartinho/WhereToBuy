d0fc837bc61b58cd08008f63d52b430732ba00c7
martasmartinho <martasmartinho@gmail.com>
2017-10-22 23:10:22 +0100

    20171022_Marta_Produtos1

--- WhereToBuy/WhereToBuy.entities/Quotations/Quotation.cs
+++ WhereToBuy/WhereToBuy.entities/Quotations/Quotation.cs
@@ -6,8 +6,17 @@ using System.Threading.Tasks;
 
 namespace WhereToBuy.entities
 {
+    [Serializable]
     public class Quotation
     {
+        private int myVar;
+
+        public int MyProperty
+        {
+            get { return myVar; }
+            set { myVar = value; }
+        }
+        
         public Quotation() { }
     }
 }
